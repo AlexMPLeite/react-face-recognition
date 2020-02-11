d8ed3fa0612bfeef10862a64c1c1ef905e777d60
Alexandre Leite <alexandre.leite@galp.com>
2020-02-11 18:24:16 +0000

    Updates

---
+++ precache-manifest.667d1511c9ea040474427abb2fa507de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83bd3438d563ba714cbd618a776bdd2b",
+    "url": "/react-face-recognition/index.html"
+  },
+  {
+    "revision": "a66900fe5b9f73068ca0",
+    "url": "/react-face-recognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "cb63ec469a79b964aa8f",
+    "url": "/react-face-recognition/static/css/main.6af165cc.chunk.css"
+  },
+  {
+    "revision": "a66900fe5b9f73068ca0",
+    "url": "/react-face-recognition/static/js/2.c440db5b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-face-recognition/static/js/2.c440db5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb63ec469a79b964aa8f",
+    "url": "/react-face-recognition/static/js/main.3ae35738.chunk.js"
+  },
+  {
+    "revision": "2539e02e1e22efcc2f2e",
+    "url": "/react-face-recognition/static/js/runtime-main.14ca336b.js"
+  }
+]);
